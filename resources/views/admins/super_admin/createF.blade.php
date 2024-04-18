e93fc096dd2d2fa57ae7e181761088687ac75e79
Aymen <2004100400155@ofppt-edu.ma>
2024-04-18 12:08:22 +0100

    add dropdow
    fillere admin

--- resources/views/admins/super_admin/createF.blade.php
+++ resources/views/admins/super_admin/createF.blade.php
@@ -12,10 +12,21 @@
             <label for="prenom">Prénom:</label>
             <input type="text" id="prenom" name="prenom" required>
         </div>
-        <div>
-            <label for="region">Region:</label>
-            <input type="text" id="region" name="region" required>
-        </div>
+        <select name="region" id="region">
+            <option value="">Select Region</option>
+            <option value="Casablanca-Settat">Casablanca-Settat</option>
+            <option value="Marrakech-Safi">Marrakech-Safi</option>
+            <option value="Rabat-Salé-Kénitra">Rabat-Salé-Kénitra</option>
+            <option value="Fès-Meknès">Fès-Meknès</option>
+            <option value="Tanger-Tétouan-Al Hoceïma">Tanger-Tétouan-Al Hoceïma</option>
+            <option value="Oriental">Oriental</option>
+            <option value="Beni Mellal-Khénifra">Beni Mellal-Khénifra</option>
+            <option value="Souss-Massa">Souss-Massa</option>
+            <option value="Guelmim-Oued Noun">Guelmim-Oued Noun</option>
+            <option value="Laâyoune-Sakia El Hamra">Laâyoune-Sakia El Hamra</option>
+            <option value="Dakhla-Oued Ed-Dahab">Dakhla-Oued Ed-Dahab</option>
+            <option value="Drâa-Tafilalet">Drâa-Tafilalet</option>
+        </select>
         <div>
             <label for="fillier">Filière:</label>
             <input type="text" id="fillier" name="fillier" required>
